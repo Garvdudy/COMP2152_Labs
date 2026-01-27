c2c3c6965b116381016e9886d63c333566c2bc12
Garvdudy <garvdudy47@gmail.com>
2026-01-27 11:30:58 -0500

    Adding Week03 files

---
+++ Week03/Q2.py
@@ -0,0 +1,21 @@
+print("=" * 50)
+print("Question 2; Shopping Cart")
+print("=" * 50)
+
+cart = ["apple", "banana", "milk", "bread", "apple", "eggs"]
+
+apple_count = cart.count("apple")
+print("Number of apples: ", apple_count)
+
+milk_position = cart.index("milk")
+print("Position of milk: ", milk_position)
+
+cart.remove("apple")
+
+# POP an item
+removed_item = cart.pop()
+print("Removed item using pop: ", removed_item)
+
+print("Is banana in the cart? ", "banana" in cart) #**
+print("Final cart; ", cart)
+print()
